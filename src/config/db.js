27361773d240f8d3372ea6ebf2ca5aec2bbbbfdc
silkpanda <anthony.ha90@gmail.com>
2025-09-11 00:20:09 -0500

    updated database connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', true);
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/familiflow_multi');
+       const conn = await mongoose.connect(process.env.DATABASE_URL);
         console.log(`MongoDB Connected successfully.`);
     } catch (error) {
         console.error(`MongoDB Connection Error: ${error.message}`);
